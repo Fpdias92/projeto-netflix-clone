aebac8b898610b502246c609a199a6a90a79ec34
fpdias92 <fpdias92@outlook.com>
2021-04-15 05:56:19 -0300

    Alterações referentes a aula 7

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        900:{
+            items:5
+        },
+        1400:{
+            items:8
+        }
+    }
+})
